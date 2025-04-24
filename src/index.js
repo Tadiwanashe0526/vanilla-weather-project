21aa5347d3692dd66c30a544d1d737c0d8c23cc0
Tadiwanashe0526 <130979445+Tadiwanashe0526@users.noreply.github.com>
2025-04-25 05:44:40 +0800

    made some changes

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ function refreshWeather(response) {
 
   descriptionElement.innerHTML = response.data.condition.description;
   timeElement.innerHTML = formatDate(date);
-  humidityElement.innerHTML = "$(response.data.temperature.humidity)%";
-  windSpeedElement.innerHTML = "$(response.data.wind.speed}Km/hr";
+  humidityElement.innerHTML = `$(response.data.temperature.humidity)%`;
+  windSpeedElement.innerHTML = `$(response.data.wind.speed}Km/hr`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
@@ -35,16 +35,15 @@ function formatDate(date) {
   let day = days[date.getDay()];
 
   if (minutes < 10) {
-    minutes = "0${ minutes }";
+    minutes = `0${minutes}`;
   }
 
-  return "${day} ${time}:${minutes}";
+  return `${day} ${time}:${minutes}`;
 }
 
 function searchCity(city) {
   let apiKey = "c37tf205aa0042d5b7b31f8a2o1dc99b";
-  let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
